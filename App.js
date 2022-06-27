48b2f638a744113ec3039f86834d11e8d6da98cc
douglasmpereira <d-moreira18@hotmail.com>
2022-06-27 11:59:15 -0300

    adicionando componentes flex

--- App.js
+++ App.js
@@ -3,6 +3,12 @@ import { StyleSheet, Text, View, Button, TouchableOpacity, TextInput } from 'rea
 import styles from './styles';
 import { useState } from 'react';
 import { ContadorRegressivo } from './componentes/contadorRegressivo/ContadorRegressivo';
+import { Ex1 } from './componentes/flex/ex1/ex1';
+import { Ex2 } from './componentes/flex/ex2/ex2';
+import { Ex3 } from './componentes/flex/ex3/ex3';
+import { Ex4 } from './componentes/flex/ex4/ex4';
+import { Ex5 } from './componentes/flex/ex5/ex5';
+import { Ex6 } from './componentes/flex/ex6/ex6';
 
 
 
@@ -15,7 +21,13 @@ export default function App() {
  
  
   return ( 
-    <ContadorRegressivo/>
+    // <ContadorRegressivo/>
+    // <Ex1/>
+    // <Ex2/>
+    // <Ex3/>
+    // <Ex4/>
+    // <Ex5/>
+    <Ex6/>
   );
 }
 
---
+++ componentes/flex/ex1/ex1.js
@@ -0,0 +1,7 @@
+import { View } from "react-native"
+
+export const Ex1 = ()=> {
+    return (
+        <View style={{ width: '40%', height:'100%', backgroundColor: 'red', alignSelf: 'center'}}/>
+    )
+}
